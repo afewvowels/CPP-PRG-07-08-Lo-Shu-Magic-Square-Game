494603100a174bc5aa9c88c69c7dbf2c1f7e9e51
Keith Smith <aeiouandsometimesy@me.com>
2017-10-08 11:44:30 -0400

    Added prompt, still needs major rework.

--- PRG-7-8-Lo-Shu-Magic-Square-Game/main.cpp
+++ PRG-7-8-Lo-Shu-Magic-Square-Game/main.cpp
@@ -5,6 +5,16 @@
 //  Created by Keith Smith on 10/8/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  The Lo Shu Magic Square is a grid with 3 rows and 3 columns as shown in figure 7-19.
+//  The Lo Shu Magic Square has the following properties:
+//
+//  * The grid contains the numbers 1 through 9 exactly.
+//  * The sum of each row, each column, and each diagonal all add up to the same number.
+//    This is shown in figure 7-20.
+//
+//  In a program, you can simulate a magic square using a two-dimensional array. Write a
+//  function that accepts a two-dimensional array as an argument, and determines whether
+//  the array is a Lo Shu Magic Square. Test the function in a program.
 
 #include <iostream>
 
